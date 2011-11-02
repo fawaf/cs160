8ef5df77616dd54ad880c5373097f89c319bb8b5
Felix Wong <felix@fawong.com>
2011-11-02 04:19:50 -0700

    changed to uppercase "Health"

--- friendhealth/src/edu/berkeley/cs160/teamk/FriendhealthActivity.java
+++ friendhealth/src/edu/berkeley/cs160/teamk/FriendhealthActivity.java
@@ -4,7 +4,7 @@ import edu.berkeley.cs160.R;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class FriendhealthActivity extends Activity {
+public class FriendHealthActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
