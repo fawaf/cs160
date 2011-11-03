6a06139d51ac90904dbcdda89725e4ee57e7e3bf
Tai Schuller <tai.schuller@gmail.com>
2011-11-02 20:13:18 -0700

    change something in FriendHealthActivity.java to appease eclipse.

--- friendhealth/gen/com/facebook/android/R.java
+++ friendhealth/gen/com/facebook/android/R.java
@@ -15,6 +15,9 @@ public final class R {
         public static final int facebook_icon=0x7f020001;
         public static final int ic_launcher=0x7f020002;
     }
+    public static final class id {
+        public static final int linearLayout1=0x7f050000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
