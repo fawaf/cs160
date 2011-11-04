7fe72057254c029bd96ec14505b20ca52d283d1b
Felix Wong <felix@fawong.com>
2011-11-04 02:59:03 -0700

    kind of fixed Ballyhoo

--- friendhealth/src/edu/berkeley/cs160/teamk/BallyhooActivity.java
+++ friendhealth/src/edu/berkeley/cs160/teamk/BallyhooActivity.java
@@ -4,16 +4,18 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.facebook.android.*;
-import com.facebook.android.Facebook.*;
 
 public class BallyhooActivity extends Activity {
+	Button fH_button;
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,17 +33,25 @@ public class BallyhooActivity extends Activity {
 		}
 		//setContentView(R.layout.ballyhooactivity);
 		
-		EditText edt_Message = (EditText) findViewById(R.id.edt_Message);
-		String message = edt_Message.getText().toString();
-		SharedPreferences mPref = getSharedPreferences("LoginActivity", MODE_PRIVATE);
-		
-		try {
-			Bundle bundle = new Bundle();
-			bundle.putString(message, "test update");
-			bundle.putString(Facebook.TOKEN, mPref.getString("access_token", null));
-			String response = Utility.facebook.request("me/feed", bundle, "POST");
-		} catch (MalformedURLException e) {
-		} catch (IOException e) {
-		}
+		//---get the fH Activity button---
+		fH_button = (Button) findViewById(R.id.btn_Invite);
+        
+        //---event handler for the fH Activity button---
+        fH_button.setOnClickListener(new View.OnClickListener() {
+        	public void onClick(View view) {
+        		try {
+        			EditText edt_Message = (EditText) findViewById(R.id.edt_Message);
+        			String message = edt_Message.getText().toString();
+        			SharedPreferences mPref = getSharedPreferences("LoginActivity", MODE_PRIVATE);
+        			Bundle bundle = new Bundle();
+        			bundle.putString("message", message);
+        			bundle.putString(Facebook.TOKEN, mPref.getString("access_token", "aelkgjalwekgj"));
+        			Log.d("fh",""+mPref.getString("access_token", "aelkgjalwekgj"));
+        			/*String response =*/ Utility.facebook.request("me/feed", bundle, "POST");
+        		} catch (MalformedURLException e) {
+        		} catch (IOException e) {
+        		}
+        	}
+        });
 	}
 }
