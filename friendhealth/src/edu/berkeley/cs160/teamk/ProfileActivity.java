98bbbabbbaa9650ce8ebe685e102f63593bb7727
Felix Wong <felix@fawong.com>
2011-12-03 03:49:49 -0800

    made ProfileActivity not calculate total score and for marketplace-ness

--- friendhealth/src/edu/berkeley/cs160/teamk/ProfileActivity.java
+++ friendhealth/src/edu/berkeley/cs160/teamk/ProfileActivity.java
@@ -40,7 +40,6 @@ public class ProfileActivity extends Activity {
 			JSONObject obj = Util.parseJson(jsonUser);
 			String facebookId = Utility.mPrefs.getString("facebookUID", "");
 			Log.d("friendHealthPA", "Facebook UID is: " + facebookId);
-			Utility.scoresDBAdapter.calculateUserTotalScore(facebookId);
 			ImageView user_picture = (ImageView) findViewById(R.id.profilePic);
 		    URL img_value = new URL("http://graph.facebook.com/" + facebookId + "/picture?type=large");
 		    Bitmap mIcon1 = BitmapFactory.decodeStream(img_value.openConnection().getInputStream());
